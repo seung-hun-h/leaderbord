3ab82473691c8c1417c66ed3eb9b268b3b1933c1
seunghun.ham <seunghun.ham@nts-corp.com>
2024-03-20 17:21:57 +0900

    init

---
+++ src/main/kotlin/org/example/runningleaderboard/RunningLeaderBoardApplication.kt
@@ -0,0 +1,11 @@
+package org.example.runningleaderboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RunningLeaderBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<RunningLeaderBoardApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/runningleaderboard/RunningLeaderBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.runningleaderboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RunningLeaderBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
